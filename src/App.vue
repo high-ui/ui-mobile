04cedfba7b886e751e881bbc7050662e1956a947
lexloo@126.com <lexloo@126.com>
2018-04-05 15:21:53 +0800

    add group

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,53 @@
 <template>
-  <div id="app">
-    <Chart :config="config"></Chart>
-    <InputItem placeholder="that is very good" label="测试"/>
-    <InputItem placeholder="that is very good" label="测试测试测试"/>
-    <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
-    <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
-    <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
-    <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
-  </div>
+    <div id="app">
+        <Chart :config="config"></Chart>
+        <InputItem placeholder="that is very good" label="测试"/>
+        <Group>
+            <InputItem placeholder="that is very good" label="测试测试测试"/>
+            <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
+            <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
+        </Group>
+        <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
+        <SelectItem placeholder="that is very good" label="测试" :options="options"></SelectItem>
+    </div>
 </template>
 
 <script>
 import InputItem from './components/input';
 import SelectItem from './components/select';
+import Group from './components/group';
 import Chart from './components/chart';
 
 export default {
-  name: 'App',
-  data() {
-  	return {
-  		config: {
-        type: 'interval',
-        position: 'genre*sold',
-        color: 'genre'
-      },
-      options: [{name:'A', value: 'A'},{name:'B', value: 'B'},{name:'C', value: 'C'},{name:'D', value: 'D'}]
-  	}
-  },
-  components: {
-    InputItem,
-    SelectItem,
-    Chart
-  }
+    name: 'App',
+    data() {
+        return {
+            config: {
+                type: 'interval',
+                position: 'genre*sold',
+                color: 'genre'
+            },
+            options: [{
+                name: 'A',
+                value: 'A'
+            }, {
+                name: 'B',
+                value: 'B'
+            }, {
+                name: 'C',
+                value: 'C'
+            }, {
+                name: 'D',
+                value: 'D'
+            }]
+        }
+    },
+    components: {
+        InputItem,
+        SelectItem,
+        Group,
+        Chart
+    }
 }
 </script>
 
@@ -39,17 +55,17 @@ export default {
 @import "./style/themes/default.less";
 
 #app {
-  background-color: @fill-body;
+    background-color: @fill-body;
 }
 
 html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
 }
 
 body {
-  background-color: @fill-body;
+    background-color: @fill-body;
 }
 </style>
---
+++ src/components/group/Group.vue
@@ -0,0 +1,59 @@
+<template>
+	<div :class="groupClasses">
+		<div v-if="showTitle"><span>{{title}}</span><a>+</a></div>
+		<slot></slot>
+	</div>
+</template>
+<script>
+	const cssPrefix = 'ui';
+	const rowPrefix = 'ui-row'
+	const prefixCls = 'ui-group';
+
+	export default {
+		name: 'Group',
+		data() {
+			return {
+			}
+		},
+		props: {
+			multiRow: {
+				type: Boolean,
+				default: false
+			},
+			title: {
+				type: String
+			}
+		},
+		computed: {
+			groupClasses() {
+				return [
+					`${rowPrefix}`,
+					`${prefixCls}`
+				];
+			},
+			showTitle() {
+				return !!this.title
+			}
+		},
+		methods: {
+		}
+	}
+</script>
+<style lang="less">
+	@import "../../style/themes/default.less";
+	@row-prefix-cls: ~"@{css-prefix}-row";
+	@group-prefix-cls: ~"@{css-prefix}-group";
+
+	.@{group-prefix-cls} {
+		.@{row-prefix-cls} {
+			margin-bottom: 0;
+		}
+
+		.@{row-prefix-cls}:after {
+			content: "";
+    		width: 100%;
+    		border-bottom: 1px solid @fill-body;
+    		position: absolute;
+		}
+	}
+</style>
